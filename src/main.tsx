44eb5f908314116a52367ce74ccd7a3101ac88c7
Ivan Minich <vane4ko@gmail.com>
2025-04-10 18:14:05 +0300

    sprint-6/step-1-v3

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./services/app/store.ts";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
